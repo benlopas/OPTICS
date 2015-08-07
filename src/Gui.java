64b352cfa684aa980b93187a3ba780f35904cf70
BEN LOPAS <19922921@student.uwa.edu.au>
2015-08-07 14:27:40 +0800

    Java Optics
    Drop Down Menu

---
+++ src/Gui.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Gui extends JFrame{
+
+        
+        private JComboBox box;
+        private JLabel picture;
+        
+        private static String[] filename = {"Laser.png","Modulator.png","Mirror.png","Link.png","Fibre Link.png"};
+        private Icon[] pics = {new ImageIcon(getClass().getResource(filename[0])),
+                           new ImageIcon(getClass().getResource(filename[1])),
+                           new ImageIcon(getClass().getResource(filename[2])),
+                           new ImageIcon(getClass().getResource(filename[3])),
+                           new ImageIcon(getClass().getResource(filename[4]))};
+        
+                           
+        public Gui(){
+          super("New Piece");
+          setLayout(new FlowLayout());
+        
+          box = new JComboBox(filename);
+        
+          box.addItemListener(
+                new ItemListener(){
+                    public void itemStateChanged(ItemEvent event){
+                        if(event.getStateChange()==ItemEvent.SELECTED)
+                            picture.setIcon(pics[box.getSelectedIndex()]);
+                        }
+                  }
+                );
+                
+                add(box);
+                picture=new JLabel(pics[0]);
+                add(picture);
+                
+             }
+            
+        }
---
+++ src/OPTICS.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class OPTICS
+{
+    public static void main(String[] args) {
+        
+        Gui go = new Gui();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(1200,600);
+        go.setVisible(true);
+    }
+}
